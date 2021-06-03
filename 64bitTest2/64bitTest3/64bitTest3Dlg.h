3a0e51c8bd85bc196649e1f2b140dc906eba5eff
hdhmask <hdhmask@gmail.com>
2021-06-04 01:42:51 +0900

    git test

---
+++ 64bitTest2/64bitTest3/64bitTest3Dlg.h
@@ -0,0 +1,31 @@
+// 64bitTest3Dlg.h : 헤더 파일
+//
+
+#pragma once
+
+
+// CMy64bitTest3Dlg 대화 상자
+class CMy64bitTest3Dlg : public CDialog
+{
+// 생성입니다.
+public:
+	CMy64bitTest3Dlg(CWnd* pParent = NULL);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+	enum { IDD = IDD_MY64BITTEST3_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
---
+++ 64bitTest2/SS360Lib/SS360Lib.h
@@ -0,0 +1,27 @@
+// SS360Lib.h : SS360Lib DLL의 기본 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'stdafx.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CSS360LibApp
+// 이 클래스의 구현을 보려면 SS360Lib.cpp를 참조하십시오.
+//
+
+class CSS360LibApp : public CWinApp
+{
+public:
+	CSS360LibApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+	DECLARE_MESSAGE_MAP()
+};
